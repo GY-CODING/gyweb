b0bf947e26d52eb23c7efe014e8efba07d8ad8b4
gfigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2023-12-28 21:25:03 +0100

    project-card page done

---
+++ src/components/ProjectsPage.jsx
@@ -0,0 +1,49 @@
+import { GlowCapture, Glow } from "@codaworks/react-glow"
+
+import fotg_card from "../assets/img/fotg_card_bg.jpg"
+import maps_card from "../assets/img/maps_card_bg.jpg"
+import characters_card from "../assets/img/characters_card_bg.jpg"
+
+import fotg_logo from "../assets/img/fotg_logo.webp"
+import maps_logo from "../assets/img/gfigueras_logo.png"
+import characters_logo from "../assets/img/toxyc_logo.png"
+
+
+import '../styles/ProjectsPage.css'
+
+export function ProjectsPage() {
+    return (
+        <GlowCapture className="projects">
+            <header className="projectsTitle">
+                <h1>PROJECTS</h1>
+            </header>
+
+            <div className="projectsList">
+                <Glow>
+                    <div className="projectCard fotgCard">
+                        <img src={fotg_logo} className="fotgLogo projectLogo" draggable="false"/>
+                        <img src={fotg_card} className="fotgBG projectBG" alt="fotg" draggable="false"/>
+                    </div>
+                </Glow>
+                <Glow>
+                    <div className="projectCard mapsCard">
+                    <img src={maps_logo} className="mapsLogo projectLogo" draggable="false"/>
+                        <img src={maps_card} className="mapsBG projectBG" alt="fotg" draggable="false"/>
+                    </div>
+                </Glow>
+                <Glow>
+                    <div className="projectCard charactersCard">
+                    <img src={characters_logo} className="charactersLogo projectLogo" draggable="false"/>
+                        <img src={characters_card} className="charactersBG projectBG" alt="fotg" draggable="false"/>
+                    </div>
+                </Glow>
+                <Glow>
+                    <div className="projectCard isaacCard">
+                    <img src={fotg_logo} className="isaacLogo projectLogo" draggable="false"/>
+                        <img src={fotg_card} className="isaacBG projectBG" alt="fotg" draggable="false"/>
+                    </div>
+                </Glow>
+            </div>
+        </GlowCapture>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Navbar } from './components/Navbar'
 import { MainPage } from './components/MainPage'
+import { ProjectsPage } from './components/ProjectsPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <MainPage />
+    <ProjectsPage />
   </React.StrictMode>,
 )
